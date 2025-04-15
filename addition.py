51d3485c57f99d640d6cac57733237f16cb23dcb
Brittany Halterman <halterma@gmail.com>
2025-04-15 15:45:59 -0400

    Try again

--- addition.py
+++ addition.py
@@ -1,18 +1,9 @@
 # create an integer variable for x
-x = 0
 # create an integer variable for y
-y = 0
 # create an integer variable for sum
-sum = 0
-
 # ask the user "X: " and put answer in x
-x = input("X: ")
 # ask the user "Y: " and put answer in y
-y = input("Y: ")
-
+# convert x to integer
+# convert y to integer
 # put x + y in sum
-sum = x + y
-
 # tell user "answer is " sum
-print ("Answer is: ")
-print (sum)
