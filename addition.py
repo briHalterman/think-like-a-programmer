8544c35b3c22f3029007f89611abab48ca705fab
Brittany Halterman <halterma@gmail.com>
2025-04-15 15:18:05 -0400

    Type code-less algorithm in comments

---
+++ addition.py
@@ -0,0 +1,7 @@
+# create an integer variable for x
+# create an integer variable for y
+# create an integer variable for sum
+# ask the user "X: " and put answer in x
+# ask the user "Y: " and put answer in y
+# put x + y in sum
+# tell user "answer is " sum
